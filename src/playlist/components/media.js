3cbe086b68ef01b97adb42d8d2b5c12218c2ff64
Danielapariona <daniela.parionac@gmail.com>
2018-03-13 12:41:30 -0500

    Add styles to the Media component

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import './media.css'
 
 class Media extends Component {
   render() {
+    const styles = {
+      container: {
+        color: '#44546b',
+        cursor: 'pointer',
+        width: 260,
+        border: '1px solid red'
+      }
+    }
+    
     return (
-      <div>
-        <div>
-          <img 
-            src="" 
+      <div className="Media">
+        <div className="Media-cover">
+          <img
+            src="./images/covers/bitcoin.jpg"
             alt=""
-            with={260}
+            width={260}
             height={160}
+            className="Media-image"
           />
-          <h3>¿Por qué aprender React?</h3>
-          <p>Daniela Pariona</p>
+          <h3 className="Media-title">¿Por qué aprender bitcoin?</h3>
+          <p className="Media-author">Daniela Pariona</p>
         </div>
       </div>
     )
