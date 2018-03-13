807336a4897f1ae77c38ea021b0bd81a25d505e7
Danielapariona <daniela.parionac@gmail.com>
2018-03-13 12:06:10 -0500

    Create Media component

---
+++ src/playlist/components/media.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Media extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img 
+            src="" 
+            alt=""
+            with={260}
+            height={160}
+          />
+          <h3>¿Por qué aprender React?</h3>
+          <p>Daniela Pariona</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Media;
