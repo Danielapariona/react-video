d5a3045a0a66b51724eddc29e4e024ef4151236d
Danielapariona <daniela.parionac@gmail.com>
2018-03-18 13:45:34 -0500

    Creating functional component of Media.js

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react'
-import Media from './media'
-import './playlist.css'
+import React from "react";
+import Media from "./media";
+import "./playlist.css";
 
-class Playlist extends Component {
-  render() {
-    const categories = this.props.data.categories
-    categories.map((item)=>{
-      console.log(item.id);
-    })
+function Playlist(props) {
+  const categories = props.data.categories;
+  categories.map(item => {
+    console.log(item.id);
+  });
 
-    // console.log(categories);
-    const playlist = this.props.data.categories[0].playlist
-    return (
-      <div className="Playlist">
-        {
-          playlist.map((item) => {
-            return <Media {...item} key={item.id} />
-          })
-        }
-      </div>
-    )
-  }
+  const playlist = props.data.categories[0].playlist;
+  return (
+    <div className="Playlist">
+      {playlist.map(item => {
+        return <Media {...item} key={item.id} />;
+      })}
+    </div>
+  );
 }
 
 export default Playlist;
