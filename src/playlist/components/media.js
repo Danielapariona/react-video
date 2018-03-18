5305f068da46499b486be887e5b543071b6be11c
Danielapariona <daniela.parionac@gmail.com>
2018-03-18 13:36:09 -0500

    Pure component

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import './media.css'
 
-class Media extends Component {
+class Media extends PureComponent {
   // Estado de la aplicación
   state = {
     author: 'Daniela Pariona C.'
