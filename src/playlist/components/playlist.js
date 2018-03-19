0789ac14c0d8b3ca8d9ec1ff6546f1a3fed197fd
Danielapariona <daniela.parionac@gmail.com>
2018-03-18 19:22:37 -0500

    Composition of components

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Media from "./media";
 import "./playlist.css";
+import Play from '../../icons/components/play'
 
 function Playlist(props) {
   const categories = props.data.categories;
@@ -11,6 +12,7 @@ function Playlist(props) {
   const playlist = props.data.categories[0].playlist;
   return (
     <div className="Playlist">
+      <Play size='100' color='red'/>
       {playlist.map(item => {
         return <Media {...item} key={item.id} />;
       })}
