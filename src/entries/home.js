c2fc7fb0b45df303c90b81da599687943463a02d
Danielapariona <daniela.parionac@gmail.com>
2018-03-18 20:34:01 -0500

    Structure of the project with Containers and UI components: layout of our website

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,9 +1,10 @@
 import React from 'react';  // Permite crear componentes
 import { render } from 'react-dom'; // Permite renderizar elementos o componentes en el navegador
-import Playlist from './src/playlist/components/playlist';
-import data from './src/api.json'
+import Home from '../pages/containers/home'
+// import Playlist from './src/playlist/components/playlist';
+// import data from './src/api.json'
 
 const app = document.getElementById('app');
 
 // ReactDOM.render(que voy a renderizar, donde lo haré);
-render(<Playlist data={data} />, app);
+render(<Home />, app);
