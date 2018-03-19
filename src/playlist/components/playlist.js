070c363a3fb9b97aff2f1d8579bb1f45340992fb
Danielapariona <daniela.parionac@gmail.com>
2018-03-18 19:42:52 -0500

    Composition of all components

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -2,6 +2,9 @@ import React from "react";
 import Media from "./media";
 import "./playlist.css";
 import Play from '../../icons/components/play'
+import FullScreen from '../../icons/components/full-screen'
+import Volume from '../../icons/components/volume'
+import Pause from '../../icons/components/pause'
 
 function Playlist(props) {
   const categories = props.data.categories;
@@ -12,7 +15,10 @@ function Playlist(props) {
   const playlist = props.data.categories[0].playlist;
   return (
     <div className="Playlist">
-      <Play size='100' color='red'/>
+      <Play size='50' color='red'/>
+      <FullScreen size='50' color='green'/>
+      <Volume size='50' color='green'/>
+      <Pause size='50' color='green'/>
       {playlist.map(item => {
         return <Media {...item} key={item.id} />;
       })}
